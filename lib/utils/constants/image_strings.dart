942f7f04ec42d7b58c5bb9ace16134de14881ccc
Edil-Ayehu <125575950+Edil-Ayehu@users.noreply.github.com>
2024-03-08 17:29:13 +0300

    Splash Screen, OnboardingScreen & Login Screen Implemented

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,9 +1,14 @@
 class TImages {
   // App logos
-  static const String darkAppLogo = "assets/logo/";
-  static const String lightAppLogo = "assets/logo/";
+  static const String darkAppLogo = "assets/logos/logo.png";
+  static const String lightAppLogo = "assets/logos/logo.png";
 
-    // Social logos
-  static const String google = "assets/logo/";
-  static const String facebook = "assets/logo/";
+  // Social logos
+  static const String google = "assets/logos/google.png";
+  static const String facebook = "assets/logos/facebook.png";
+
+  // Onboarding images
+  static const String onBoardingImage1 = "assets/images/onboarding_images/intro2.png";
+  static const String onBoardingImage2 = "assets/images/onboarding_images/intro3.png";
+  static const String onBoardingImage3 = "assets/images/onboarding_images/intro5.png";
 }
