1483838b49e4168f12290eaa3c798a005ef2f371
Edil-Ayehu <125575950+Edil-Ayehu@users.noreply.github.com>
2024-03-08 11:44:21 +0300

    theme & utility setup completed!

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // App logos
+  static const String darkAppLogo = "assets/logo/";
+  static const String lightAppLogo = "assets/logo/";
+
+    // Social logos
+  static const String google = "assets/logo/";
+  static const String facebook = "assets/logo/";
+}
